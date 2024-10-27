bc007b0beaa3f6d6cc4c1f0448991fc51c4c5a7c
manuel <manuelfelipe1713@gmail.com>
2024-10-26 22:56:59 -0500

    Jerarquia de clases y métodos

--- src/com/curso/screenmacth/modelos/Pelicula.java
+++ src/com/curso/screenmacth/modelos/Pelicula.java
@@ -17,4 +17,9 @@ public class Pelicula extends Titulo implements Clasificable {
     public int getClasificable() {
         return (int) (calculaMedia() / 2);
     }
+
+    @Override
+    public String toString() {
+        return "Pelicula: " + this.getNombre() + " (" + getFechaDeLanzamiento() +")";
+    }
 }
